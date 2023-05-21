06b509c6e8bb552048db7c9fef127cb05a00ae27
CamillDGI <cam.ddeguzman@gmail.com>
2023-05-21 14:00:23 +0800

    May 21 1st

---
+++ swiftware/resources/views/admin.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layouts')
+
+@section('admin_dashboard')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="content">
+        <div class="title m-b-md">
+            Swiftware Warehouse Solutions
+        </div>            
+    </div>
+</div>
+
+@endsection
+
+
+
